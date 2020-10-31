a5307d8f2f4eaa2bd0c34bbc4f367e64a04be7a9
nazar-java-dev <agajanovh@gmail.com>
2020-10-30 22:50:38 -0400

    SpringBoot-Demo

---
+++ Spring-06-JavaCode-PostConstruct-Predestroy/src/main/java/com/cybertek/config/CybertekAppConfig.java
@@ -0,0 +1,13 @@
+package com.cybertek.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+@Configuration
+@ComponentScan("com.cybertek")
+public class CybertekAppConfig {
+
+}
