4ce6f1ffbfe3ea87120928752fd69afb3abfc6c5
nazar-java-dev <agajanovh@gmail.com>
2020-11-10 20:26:03 -0500

    created springboot controller and view end point

---
+++ Spring-08-SpringBoot-MVC-Controller/src/main/java/com/cybertek/controller/HomeController.java
@@ -0,0 +1,36 @@
+package com.cybertek.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String getRequestMapping(){
+        return "home";
+    }
+
+    @RequestMapping(method = RequestMethod.GET,value = "/cybertek")
+    public String getRequestMapping2(){
+        return "home";
+    }
+
+    @RequestMapping(method = RequestMethod.GET,value = "/ozzy")
+    public String getRequestMapping3(){
+        return "home";
+    }
+
+    @GetMapping("/spring")
+    public String getMappingEx(){
+        return "home";
+    }
+
+    @PostMapping("/ozzy")
+    public String getMappingEx2(){
+        return "home";
+    }
+}
